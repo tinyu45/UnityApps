fb34508366872095ea9da0bd9f51d3152e2d9961
timy <tinyu45O@163.com>
2018-01-18 08:45:40 +0800

    二次提交

---
+++ PlanesWar/Assets/BaovoxController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class BaovoxController : MonoBehaviour {
+
+	float delTime=0;
+	// Use this for initialization
+	void Start () {
+		this.transform.position = new Vector3(Random.Range(-2.5f, 2.5f),Random.Range(-1.5f, 2.5f),0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		delTime += Time.deltaTime;
+		if (delTime >= 15) {
+			Destroy (this.gameObject);  //三秒无人捡起，销毁
+		}
+	}
+
+	public void OnTriggerEnter2D(Collider2D col2D){
+		if (col2D.gameObject.tag == "Plane") {
+			Destroy (this.gameObject);
+			GameController.hasReword = true;
+			GameController.reward++;
+			//print ("嘿嘿，捡到宝啦！");
+		}
+	}
+}
